df86b7bcbf5fe681bb006429a0d250194e3d855a
Eamonn Jawhary <eamonnjawhary@gmail.com>
2021-09-23 17:42:21 +0400

    initial

---
+++ POWEReNamer.ps1
@@ -0,0 +1,51 @@
+""
+""
+"					*** POWEReNamer by Eamonn Jawhary ***"
+""
+""
+
+"Hello, I am your computer..."
+
+""
+
+Start-Sleep -s 1
+
+$hostname = Read-Host "Enter New Hostname"
+
+if ($hostname.Length -gt 15)
+{
+	"Hostnames more than 15 characters not allowed...Please try again..."
+	"EXITING"
+	Start-Sleep -s 5
+	Exit
+}
+
+""
+
+"WARNING: THIS SCRIPT WILL RESTART THIS PC AUTOMATICALLY, PLEASE ENSURE ALL WORK IS SAVED BEFORE CONTINUING..."
+
+""
+
+$continue = Read-Host "DO YOU WANT TO CONTINUE? y/n"
+
+""
+
+if ($continue.ToLower() -eq "y") 
+{
+	Rename-Computer -NewName $hostname
+	"Hostname changed to $hostname"
+	""
+	Start-Sleep -s 2
+	
+	for ($i = 10 ; $i -ge 0 ; $i--)
+	{    
+		"System restarting in $i"
+		Start-Sleep -s 1
+	}
+	
+	Restart-Computer
+} 
+else
+{
+	Exit
+}
